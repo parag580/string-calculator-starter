968fafdde85dee33a998d9a0577963939c9b8fac
D4_49930_Parag <parag580@gmail.com>
2021-10-17 23:22:01 +0530

    empty_string_return_zero

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -12,9 +12,5 @@ class StringCalculatorShould {
         assertEquals(0, stringCalculator.add(""));
     }
 
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
+   
 }
