196bea8df8a7db1ce1ca6313fb0c99b62ce26118
D4_49930_Parag <parag580@gmail.com>
2021-10-17 23:36:15 +0530

    with_two_number_return_sum

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,6 +5,10 @@ class StringCalculator {
     public int add(String input) {
     	if(input.isEmpty())
         return 0;
+    	else if(input.contains(",")) {
+    		String[] numbers=input.split(",");
+    		return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+    	}
     	else
     		return Integer.parseInt(input);
     }
