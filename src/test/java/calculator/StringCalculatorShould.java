99b5f849f0afc6a221462364ab97a88fca2129e7
D4_49930_Parag <parag580@gmail.com>
2021-10-18 01:20:34 +0530

    refactor_2

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -8,37 +8,37 @@ class StringCalculatorShould {
 
     @Test
     void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
+       
+        assertEquals(0, StringCalculator.add(""));
     }
 
     @Test
     void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
+        
+        assertEquals(1, StringCalculator.add("1"));
     }
     
     @Test
     void string_with_two_number_should_return_number_as_sum() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(3, stringCalculator.add("1,2"));
+       
+        assertEquals(3, StringCalculator.add("1,2"));
     }
     
     @Test
     void string_with_unkmown_amount_of_number_should_return_number_as_sum() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(113, stringCalculator.add("1,2,50,60"));
+       
+        assertEquals(113, StringCalculator.add("1,2,50,60"));
     }
     
     @Test
     void string_with_newline_between_number_should_return_number_as_sum() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(6, stringCalculator.add("1\n2,3"));
+       
+        assertEquals(6, StringCalculator.add("1\n2,3"));
     }
     
     @Test
     void delimiter_specified_between_number_should_return_number_as_sum() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(3, stringCalculator.add("//;\n1;2"));
+        
+        assertEquals(3, StringCalculator.add("//;\n1;2"));
     }
 }
